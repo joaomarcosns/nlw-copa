a3d84663f80db881da730d3a32533124adba390d
João Marcos Neves da Silva <joaomarcosnina@gmail.com>
2022-11-01 10:57:13 -0300

    Fim da primeira  aula

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { Center, Spinner } from "native-base";
+
+
+export const Loading = () => {
+  return (
+    <Center flex={1} bg='gray.500'>
+      <Spinner color='yellow.500'></Spinner>
+    </Center>
+  )
+}
